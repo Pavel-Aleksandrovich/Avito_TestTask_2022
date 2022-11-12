1706b1f9080288a7ed28c8efdf92fca5103a91e7
Pavel Mishanin <pavelmishanin@mail.ru>
2022-11-12 14:16:03 +0700

    Create tableView

---
+++ Avito_TestTask_2022/Avito_TestTask_2022/ListEmployees/Cell/ListEmployeesTableCell.swift
@@ -0,0 +1,72 @@
+//
+//  ListEmployeesTableCell.swift
+//  Avito_TestTask_2022
+//
+//  Created by pavel mishanin on 12.11.2022.
+//
+
+import UIKit
+
+final class ListEmployeesTableCell: UITableViewCell {
+    
+    static let id = String(describing: ListEmployeesTableCell.self)
+    
+    private let nameLabel = UILabel()
+    private let phoneNumberLabel = UILabel()
+    private let skillsLabel = UILabel()
+    private let vStackView = UIStackView()
+    
+    override init(style: UITableViewCell.CellStyle,
+                  reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configAppearance()
+        makeConstraints()
+        
+        nameLabel.text = "nameLabel"
+        
+        phoneNumberLabel.text = "phoneNumberLabel"
+        
+        skillsLabel.text = "skillsLabel"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Config Appearance
+private extension ListEmployeesTableCell {
+    
+    func configAppearance() {
+        
+        vStackView.axis = .vertical
+        vStackView.alignment = .center
+        vStackView.distribution = .fill
+        vStackView.spacing = 10
+    }
+}
+
+// MARK: - Make Constraints
+private extension ListEmployeesTableCell {
+    
+    func makeConstraints() {
+        addSubview(vStackView)
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            vStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            vStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            vStackView.topAnchor.constraint(equalTo: topAnchor),
+            vStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        
+        vStackView.addArrangedSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        vStackView.addArrangedSubview(phoneNumberLabel)
+        phoneNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        vStackView.addArrangedSubview(skillsLabel)
+        skillsLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
--- Avito_TestTask_2022/Avito_TestTask_2022/ListEmployees/ViewController/ListEmployeesViewController.swift
+++ Avito_TestTask_2022/Avito_TestTask_2022/ListEmployees/ViewController/ListEmployeesViewController.swift
@@ -14,6 +14,7 @@ protocol IListEmployeesViewController: AnyObject {
 final class ListEmployeesViewController: UIViewController {
 
     private let presenter: IListEmployeesPresenter
+    private let tableView = UITableView()
     
     init(presenter: IListEmployeesPresenter) {
         self.presenter = presenter
@@ -27,10 +28,38 @@ final class ListEmployeesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.onViewAttached(controller: self)
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ListEmployeesTableCell.self,
+                           forCellReuseIdentifier: ListEmployeesTableCell.id)
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
 extension ListEmployeesViewController: IListEmployeesViewController {
     
+}
+
+extension ListEmployeesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListEmployeesTableCell.id, for: indexPath) as? ListEmployeesTableCell else {
+            return UITableViewCell() }
+        print(indexPath.row)
+        return cell
+    }
 }
