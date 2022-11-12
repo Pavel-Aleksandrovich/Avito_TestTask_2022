1fdbbabe4128201a00f53ea7e9a974423f583175
Pavel Mishanin <pavelmishanin@mail.ru>
2022-11-12 12:14:24 +0700

    Create project

---
+++ Avito_TestTask_2022/Avito_TestTask_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito_TestTask_2022
+//
+//  Created by pavel mishanin on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
